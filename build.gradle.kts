30eb9ab9b7868ea99aebd251d176cdefb347ac71
gabrieldasilvadev <gabrieldasilvadev@gmail.com>
2024-05-11 19:08:43 -0300

    added modules for component test and arch test

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,14 +21,73 @@ repositories {
 	mavenCentral()
 }
 
+
+// Component Test
+sourceSets {
+	create("componentTest") {
+		compileClasspath += sourceSets.main.get().output + sourceSets.test.get().output
+		runtimeClasspath += sourceSets.main.get().output + sourceSets.test.get().output
+	}
+}
+val componenetTestTask = tasks.create("componentTest", Test::class) {
+	description = "Runs the component tests."
+	group = "verification"
+
+	testClassesDirs = sourceSets["componentTest"].output.classesDirs
+	classpath = sourceSets["componentTest"].runtimeClasspath
+}
+val componentTestImplementation: Configuration by configurations.getting {
+	extendsFrom(configurations.implementation.get())
+	extendsFrom(configurations.testImplementation.get())
+}
+configurations["componentTestRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+// Arch Test
+sourceSets {
+	create("archTest") {
+		compileClasspath += sourceSets.main.get().output
+		runtimeClasspath += sourceSets.main.get().output
+	}
+}
+val archTest = tasks.create("archTest", Test::class) {
+	description = "Runs the architecture tests."
+	group = "verification"
+
+	testClassesDirs = sourceSets["archTest"].output.classesDirs
+	classpath = sourceSets["archTest"].runtimeClasspath
+}
+
+val archTestImplementation: Configuration by configurations.getting {
+	extendsFrom(configurations.implementation.get())
+	extendsFrom(configurations.testImplementation.get())
+}
+configurations["archTestRuntimeOnly"].extendsFrom(configurations.runtimeOnly.get())
+
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web") {
+		modules {
+			module("org.springframework.boot:spring-boot-starter-tomcat") {
+				replacedBy("org.springframework.boot:spring-boot-starter-jetty")
+			}
+		}
+	}
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	annotationProcessor("org.projectlombok:lombok")
+
+	// Testing
+	archTestImplementation("com.tngtech.archunit:archunit:1.2.1")
+	componentTestImplementation("com.github.tomakehurst:wiremock:3.0.0")
+	componentTestImplementation("io.rest-assured:rest-assured:5.4.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
