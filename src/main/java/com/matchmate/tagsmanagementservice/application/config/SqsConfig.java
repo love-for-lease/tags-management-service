826a5fa8f0503c317d4f500d2222e480dfb02f6b
gabrieldasilvadev <gabrieldasilvadev@gmail.com>
2024-05-25 15:06:40 -0300

    fix sqs config

--- src/main/java/com/matchmate/tagsmanagementservice/application/config/SqsConfig.java
+++ src/main/java/com/matchmate/tagsmanagementservice/application/config/SqsConfig.java
@@ -1,33 +1,25 @@
 package com.matchmate.tagsmanagementservice.application.config;
 
-import io.awspring.cloud.autoconfigure.sqs.SqsProperties;
-import io.awspring.cloud.sqs.config.SqsBootstrapConfiguration;
-import io.awspring.cloud.sqs.config.SqsMessageListenerContainerFactory;
+import com.matchmate.tagsmanagementservice.application.properties.SqsProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
-@Import(SqsBootstrapConfiguration.class)
+import java.net.URI;
+
 @Configuration
+@RequiredArgsConstructor
 public class SqsConfig {
 
-    @Bean
-    public SqsMessageListenerContainerFactory<Object> defaultSqsListenerContainerFactory() {
-        return SqsMessageListenerContainerFactory
-            .builder()
-            .sqsAsyncClient(sqsAsyncClient())
-            .build();
-    }
+    private final SqsProperties properties;
 
     @Bean
     public SqsAsyncClient sqsAsyncClient() {
-        return SqsAsyncClient.builder().build();
+        return SqsAsyncClient.builder()
+                .region(Region.of(properties.getRegion()))
+                .endpointOverride(URI.create(properties.getEndpoint()))
+                .build();
     }
-
-    @Bean
-    public SqsProperties.Listener listener() {
-        return new SqsProperties.Listener();
-    }
-
 }
